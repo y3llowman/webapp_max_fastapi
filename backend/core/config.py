15406600b665321b64856255521edfe188addf59
y3llowman <myteddy062@gmail.com>
2026-09-10 23:07:41 +0300

    refactor: simplify MAX configuration

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,70 +1,16 @@
-import logging.config
-import os
-from pathlib import Path
+import logging
 
-from .env import EnvSettings
+from .env import ENV
 
-ENV = EnvSettings()  # type: ignore
-
-BASE_DIR = Path(__file__).resolve().parents[1]
-FRONTEND_DIR = BASE_DIR / "frontend" / "build"
-
-PROJECT_NAME = ENV.PROJECT_NAME
-
-DEBUG = ENV.DEBUG
-EXTERNAL_URL = str(ENV.EXTERNAL_URL)
-PORT = ENV.PORT
 HOST = ENV.HOST
+PORT = ENV.PORT
+DEBUG = ENV.DEBUG
 PROJECT_NAME = ENV.PROJECT_NAME
-
 SECRET_KEY = ENV.SECRET_KEY
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+MAX_BOT_TOKEN = ENV.MAX_BOT_TOKEN
+MAX_INIT_DATA_MAX_AGE = ENV.MAX_INIT_DATA_MAX_AGE
 
-# TELEGRAM SETTINGS
-TELEGRAM_TOKEN = ENV.TELEGRAM.TOKEN
-BOT_ADMIN_IDS = ENV.TELEGRAM.ADMINS
-TELEGRAM_SECRET = ENV.TELEGRAM.SECRET
-
-MONGO_DSN = str(ENV.MONGO_DSN)
-RABBITMQ_URL = ENV.RABBITMQ.URL
-
-# LOGGING SETTINGS
-ERROR_LOG_FILENAME = BASE_DIR / "frontend" / "error.log"
-os.makedirs(os.path.dirname(ERROR_LOG_FILENAME), exist_ok=True)
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "default": {
-            "format": "%(asctime)s:%(levelname)s:%(message)s",
-            "datefmt": "%Y-%m-%d %H:%M:%S",
-        },
-        "simple": {"format": "%(filename)s:%(lineno)d: - %(message)s"},
-    },
-    "handlers": {
-        "logfile": {
-            "class": "logging.handlers.RotatingFileHandler",
-            "level": "ERROR",
-            "filename": ERROR_LOG_FILENAME,
-            "formatter": "default",
-            "backupCount": 2,
-        },
-        "console": {
-            "class": "logging.StreamHandler",
-            "level": "DEBUG",
-            "formatter": "simple",
-            "stream": "ext://sys.stdout",
-        },
-    },
-    "loggers": {
-        PROJECT_NAME: {
-            "level": "INFO",
-            "handlers": ["console"],
-            "propagate": False,
-        },
-    },
-    "root": {"level": "INFO", "handlers": ["logfile", "console"]},
-}
-
-logging.config.dictConfig(LOGGING)
+logging.basicConfig(
+    level=logging.DEBUG if DEBUG else logging.INFO,
+    format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+)
